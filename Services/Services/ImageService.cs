d23b17e23d4e74a269a9ae94c7c38635bf47018b
Yuriy Nikitin <iyura1997@gmail.com>
2024-01-14 21:05:47 +0300

    Done svg thumbs

--- Services/Services/ImageService.cs
+++ Services/Services/ImageService.cs
@@ -29,14 +29,14 @@ namespace Services.Services
         public string GetBookImageSvg(BookGetVM bookVM, string cssClasses = "")
         {
             var textHtml = $"""
-                <span style="text-align: center;" xmlns="http://www.w3.org/1999/xhtml">
-                    <h5>{bookVM.Title}</h5>
-                    <p style="font-style: italic;">{bookVM.Author}</p>
+                <span style="text-align: center; color: #dee2e6;" xmlns="http://www.w3.org/1999/xhtml">
+                    <p style="font-size: x-large;">{bookVM.Title}</p>
+                    <p style="font-size: large; font-style: italic;">{bookVM.Author}</p>
                 </span>
                 """;
 
             return $"""
-                <svg class="{cssClasses}" height="100%" width="80%" xmlns="http://www.w3.org/2000/svg" role="img">
+                <svg class="{cssClasses}" height="400px" width="260px" xmlns="http://www.w3.org/2000/svg" role="img">
                     <title>Book cover thumbnail</title>
                     <rect fill="#7f7f7f" height="100%" width="100%" />
                     <foreignObject x="10%" y="15%" width="80%" height="100%">
