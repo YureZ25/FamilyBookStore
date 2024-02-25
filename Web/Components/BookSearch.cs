22e1a7dd91abe37f30c71d7dde197a7890fa1836
Yuriy Nikitin <iyura1997@gmail.com>
2024-02-25 17:15:52 +0300

    Add view component

---
+++ Web/Components/BookSearch.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Components
+{
+    public class BookSearch : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
