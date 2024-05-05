817787620a63bb00fc0ddb198184653421c8595d
Yuriy Nikitin <iyura1997@gmail.com>
2024-05-05 11:13:33 +0300

    Add typeachead lib

---
+++ Web/Views/Shared/Components/BookSearch/Default.cshtml.js
@@ -0,0 +1,20 @@
+﻿const books = ["muracami", "london"];
+
+const bookSource = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.whitespace,
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    local: books
+})
+
+const bookDataset = {
+    name: 'books',
+    source: bookSource
+};
+
+const options = {
+    highlight: true,
+    minLength: 3,
+    hint: true,
+};
+
+$('.typeahead').typeahead(options, bookDataset);
